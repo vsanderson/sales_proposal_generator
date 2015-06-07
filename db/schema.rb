35ef99e9187b765947707b6cad3f198a174b008b
Dave Shaffer <dave.shaffer@gmail.com>
2015-06-06 22:10:20 -0400

    😇

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606213739) do
+ActiveRecord::Schema.define(version: 20150607020816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 20150606213739) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
 
   add_foreign_key "customers", "users"
   add_foreign_key "products", "suppliers"
