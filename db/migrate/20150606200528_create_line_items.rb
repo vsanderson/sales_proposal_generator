d6c86d61bb39ceda4e4f0ae2a2cb940bbca1f1ea
Dave Shaffer <dave.shaffer@gmail.com>
2015-06-06 21:46:06 -0400

    Fix fucked up migration

--- db/migrate/20150606200528_create_line_items.rb
+++ db/migrate/20150606200528_create_line_items.rb
@@ -2,7 +2,7 @@ class CreateLineItems < ActiveRecord::Migration
   def change
     create_table :line_items do |t|
       t.references :proposal
-      t.references :product_id
+      t.references :product
       t.integer :quantity
       t.decimal :discount
       t.string :notes
